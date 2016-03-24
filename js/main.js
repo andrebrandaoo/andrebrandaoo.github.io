e7faa756200c75efdc5d9401f01271961dc88511
André Brandão <andrebf4@gmail.com>
2016-03-24 04:37:36 -0300

    add nav bar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var main = function (){
+
+  //desenho das skills
   $(function() {
     $('.skill').easyPieChart({
       scaleColor: "#ecf0f1",
@@ -10,6 +12,53 @@ var main = function (){
       size: 150,
       animate: 500
     });
+  });
+
+  $( "#menu" ).on('click touchstart', function(e) {
+    e.preventDefault();
+    if($('.toggle-icon').hasClass('fa-bars')){
+      e.preventDefault();
+
+      //troca o icon para x
+      $( '.toggle-icon' ).removeClass('fa-bars');
+      $( '.toggle-icon' ).addClass('fa-close');
+
+      //posiciona o menu na tela
+      $('.nav').animate({
+        right: "0px"
+      }, 200);
+
+      $('body').animate({
+        right: "250px"
+      }, 200);
+
+      $('.bar').animate({
+        right: "250px"
+      }, 200);
+
+    }
+    else {
+      e.preventDefault();
+
+      //troca o icone para bars
+      $( '.toggle-icon' ).removeClass('fa-close');
+      $( '.toggle-icon' ).addClass('fa-bars');
+
+      //esconde o menu da tela
+      $('.nav').animate({
+        right: "-250px"
+      }, 200);
+
+      $('body').animate({
+        right: "0px"
+      }, 200);
+
+      $('.bar').animate({
+        right: "0px"
+      }, 200);
+
+    }
+    return false;
   });
 
 };
