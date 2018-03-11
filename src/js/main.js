4d2a8d8afa6b4e7270d54ec3f9dfdf6549f93cc6
Andre Brandao <andrebf4@gmail.com>
2018-03-11 17:32:28 -0300

    send msg with mixpanel

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,10 @@ new Vue({
       currentJob: null,
       interactions: {
         modalJob: false
-      }
+      },
+      emailInvalid: false,
+      errors: [],
+      form: { name: '', email: '', msg: '', }
     }
   },
 
@@ -27,6 +30,35 @@ new Vue({
     openClickedJob: function (job) {
       this.currentJob = job
       this.interactions.modalJob = true
+    },
+
+    validInput: function (input) {
+      var index = this.errors.indexOf(event.target.id)
+
+      // Check if email is valid
+      if (event.target.id === 'email') {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        this.emailInvalid = !re.test(event.target.value)
+      }
+
+      if ((!event.target.value || this.emailInvalid) && index < 0) {
+        this.errors.push(event.target.id)
+      }
+
+      if (event.target.value && index > -1) {
+        this.errors.splice(index, 1)
+      }
+    },
+
+    submitForm: function () {
+      console.log(this.form)
+
+      mixpanel.track('Contact Form', {
+        'name': this.form.name,
+        'email': this.form.email,
+        'message': this.form.msg
+      })
     }
   }
 })
