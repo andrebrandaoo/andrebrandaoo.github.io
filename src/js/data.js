79a019b3f50cc3a4268f4621c6d7ed6cbe85bb73
Andre Brandao <andrebf4@gmail.com>
2018-03-11 01:44:34 -0300

    added vuejs and use it to list portfolio jobs

--- src/js/data.js
+++ src/js/data.js
@@ -1,12 +1,72 @@
-const portfolio = [
+var portfolio = [
+  // Menu Mais Bartendes
   {
-    name: '',
-    slug: '',
-    description_pt: '',
-    description_en: '',
-    company: '',
+    name: 'Menu Mais Bartenders',
+    slug: 'menu_mais_bartenders',
+    excerpt: 'Resumo aqui',
+    photo_url: '/src/images/portfolio/maisbartenders.png',
+    description: '<p>Menu Mais Bartendes é um aplicativo onde os convidados do evento podem intergir com o bar da Mais Bartenders, o usuário pode visualizar o cardápio completo do evento, curtir, salvar drinks e fazer comentários para o evento e drinks.</p><p>O Aplicativo foi desenvolvido utilizando o VueJS 2 e é servido por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>O aplicativo conta com Login social parafacilitar o acesso de novos usuários, existe uma integração com o Facebook e Whatsapp onde o usuário pode compartilhar comentários e enviar links para outras pessoas.</p><p>É um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    company: 'weplanner',
+    link: 'https://menu.maisbartenders.com.br',
     tasks: [
-      { name_pt: '', name_en: '' }
+      'UI', 'UX', 'FrontEnd', 'VueJS', 'HTML', 'CSS', 'JS', 'Jquery'
     ]
-  }
+  },
+
+  // Site Mais Bartendes
+  {
+    name: 'Site Mais Bartenders',
+    slug: 'site_mais_bartenders',
+    excerpt: 'Resumo aqui',
+    photo_url: '/src/images/portfolio/maisbartenders.png',
+    description: '<p>Menu Mais Bartendes é um aplicativo onde os convidados do evento podem intergir com o bar da Mais Bartenders, o usuário pode visualizar o cardápio completo do evento, curtir, salvar drinks e fazer comentários para o evento e drinks.</p><p>O Aplicativo foi desenvolvido utilizando o VueJS 2 e é servido por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>O aplicativo conta com Login social parafacilitar o acesso de novos usuários, existe uma integração com o Facebook e Whatsapp onde o usuário pode compartilhar comentários e enviar links para outras pessoas.</p><p>É um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    company: 'weplanner',
+    link: 'https://maisbartenders.com.br',
+    tasks: [
+      'UI', 'UX', 'FrontEnd', 'Laravel', 'HTML', 'CSS', 'JS'
+    ]
+  },
+
+  // GoUp
+  {
+    name: 'GoUp',
+    slug: 'goup',
+    photo_url: '/src/images/portfolio/goup.png',
+    excerpt: 'Resumo aqui',
+    description: '<p>GoUp é um aplicativo para compartilhamento de locais onde acontecem eventos, baladas, além disso o aplicativo permite que o usuário compartilhe momentos e esses momentos são ou não curtidos por outros usuários.</p><p>O Aplicativo foi desenvolvido utilizando o VueJS 2 e é servido por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>O aplicativo utiliza geolocalização para encontrar momentos próximos ao usuário e também envia push notifications para comunicação e avisos aos usuários.</p><p>É um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    company: 'weplanner',
+    link: 'https://goup.today/',
+    tasks: [
+      'UI', 'UX', 'FrontEnd', 'VueJS', 'HTML', 'CSS', 'JS', 'Animations'
+    ]
+  },
+
+  // Isaudavel
+  {
+    name: 'Isaudavel',
+    slug: 'isaudavel',
+    photo_url: '/src/images/portfolio/isaudavel.png',
+    excerpt: 'Resumo aqui',
+    description: '<p>O mais inovador aplicativo para conectar profissionais e empresas de saúde com seus clientes!</p><p>O iSaudavel é uma Single Page Application (SPA) que foi desenvolvida utilizando o VueJS 2 e é servida por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>A plataforma é uma aplicação multi clientes (Multi Tenancy) e é fornecida como SaaS (Software as a Service).</p><p>O aplicativo utiliza geolocalização para encontrar profissionais próximos ao usuário e também envia push notifications para comunicação e avisos aos usuários.</p><p>O iSaudavel também conta com um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    company: 'weplanner',
+    link: 'https://isaudavel.com',
+    tasks: [
+      'UI', 'UX', 'FrontEnd', 'VueJS', 'HTML', 'CSS', 'JS', 'Jquery'
+    ]
+  },
+
+  // Weplaces
+  {
+    name: 'Weplaces',
+    slug: 'weplaces',
+    photo_url: '/src/images/portfolio/weplaces.png',
+    excerpt: 'Resumo aqui',
+    description: '<p>We Places é uma ferramenta para facilitar a vida de quem procura espaços de cerimônia e festas.</p><p>O We Places conta com uma Single Page Application (SPA) que foi desenvolvida utilizando o VueJS 2 e é servida por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>A plataforma é uma aplicação multi clientes (Multi Tenancy) e é fornecida como SaaS (Software as a Service).</p><p>O We Places também conta com um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    company: 'weplanner',
+    link: 'https://weplaces.com.br',
+    tasks: [
+      'UI', 'UX', 'FrontEnd', 'VueJS', 'HTML', 'CSS', 'JS', 'Jquery'
+    ]
+  },
+
 ]
---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+Vue.config.debug = true;
+
+new Vue({
+  name: 'app',
+  el: '#app',
+
+  data() {
+    return {
+
+    }
+  },
+
+  computed: {
+    portolio: function () {
+      return portfolio
+    }
+  },
+
+  mounted() {
+  },
+
+  methods: {
+
+  }
+})
