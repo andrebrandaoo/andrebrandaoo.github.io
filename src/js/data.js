5c43a6fa86feb477ffafb88b66475b638da4d57a
Andre Brandao <andrebf4@gmail.com>
2018-03-11 00:59:10 -0300

    model to portfolio items

--- src/js/data.js
+++ src/js/data.js
@@ -1,5 +1,12 @@
 const portfolio = [
   {
-    
+    name: '',
+    slug: '',
+    description_pt: '',
+    description_en: '',
+    company: '',
+    tasks: [
+      { name_pt: '', name_en: '' }
+    ]
   }
 ]
