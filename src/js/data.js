b90e38e15c11680cc81679a54285c9e88d50409a
Andre Brandao <andrebf4@gmail.com>
2018-03-11 20:38:26 -0300

    update descriptions

--- src/js/data.js
+++ src/js/data.js
@@ -3,7 +3,6 @@ var portfolio = [
   {
     name: 'Menu Mais Bartenders',
     slug: 'menu_mais_bartenders',
-    excerpt: 'Resumo aqui',
     photo_url: '/src/images/portfolio/maisbartenders.png',
     description: '<p>Menu Mais Bartendes é um aplicativo onde os convidados do evento podem intergir com o bar da Mais Bartenders, o usuário pode visualizar o cardápio completo do evento, curtir, salvar drinks e fazer comentários para o evento e drinks.</p><p>O Aplicativo foi desenvolvido utilizando o VueJS 2 e é servido por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>O aplicativo conta com Login social parafacilitar o acesso de novos usuários, existe uma integração com o Facebook e Whatsapp onde o usuário pode compartilhar comentários e enviar links para outras pessoas.</p><p>É um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
     company: 'Weplanner',
@@ -17,7 +16,6 @@ var portfolio = [
   {
     name: 'Site Mais Bartenders',
     slug: 'site_mais_bartenders',
-    excerpt: 'Resumo aqui',
     photo_url: '/src/images/portfolio/maisbartenders.png',
     description: '<p>Menu Mais Bartendes é um aplicativo onde os convidados do evento podem intergir com o bar da Mais Bartenders, o usuário pode visualizar o cardápio completo do evento, curtir, salvar drinks e fazer comentários para o evento e drinks.</p><p>O Aplicativo foi desenvolvido utilizando o VueJS 2 e é servido por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>O aplicativo conta com Login social parafacilitar o acesso de novos usuários, existe uma integração com o Facebook e Whatsapp onde o usuário pode compartilhar comentários e enviar links para outras pessoas.</p><p>É um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
     company: 'Weplanner',
@@ -32,7 +30,6 @@ var portfolio = [
     name: 'GoUp',
     slug: 'goup',
     photo_url: '/src/images/portfolio/goup.png',
-    excerpt: 'Resumo aqui',
     description: '<p>GoUp é um aplicativo para compartilhamento de locais onde acontecem eventos, baladas, além disso o aplicativo permite que o usuário compartilhe momentos e esses momentos são ou não curtidos por outros usuários.</p><p>O Aplicativo foi desenvolvido utilizando o VueJS 2 e é servido por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>O aplicativo utiliza geolocalização para encontrar momentos próximos ao usuário e também envia push notifications para comunicação e avisos aos usuários.</p><p>É um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
     company: 'Weplanner',
     link: 'https://goup.today/',
@@ -46,7 +43,6 @@ var portfolio = [
     name: 'Isaudavel',
     slug: 'isaudavel',
     photo_url: '/src/images/portfolio/isaudavel.png',
-    excerpt: 'Resumo aqui',
     description: '<p>O mais inovador aplicativo para conectar profissionais e empresas de saúde com seus clientes!</p><p>O iSaudavel é uma Single Page Application (SPA) que foi desenvolvida utilizando o VueJS 2 e é servida por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>A plataforma é uma aplicação multi clientes (Multi Tenancy) e é fornecida como SaaS (Software as a Service).</p><p>O aplicativo utiliza geolocalização para encontrar profissionais próximos ao usuário e também envia push notifications para comunicação e avisos aos usuários.</p><p>O iSaudavel também conta com um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
     company: 'Weplanner',
     link: 'https://isaudavel.com',
@@ -60,7 +56,6 @@ var portfolio = [
     name: 'Weplaces',
     slug: 'weplaces',
     photo_url: '/src/images/portfolio/weplaces.png',
-    excerpt: 'Resumo aqui',
     description: '<p>We Places é uma ferramenta para facilitar a vida de quem procura espaços de cerimônia e festas.</p><p>O We Places conta com uma Single Page Application (SPA) que foi desenvolvida utilizando o VueJS 2 e é servida por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>A plataforma é uma aplicação multi clientes (Multi Tenancy) e é fornecida como SaaS (Software as a Service).</p><p>O We Places também conta com um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
     company: 'Weplanner',
     link: 'https://weplaces.com.br',
@@ -74,8 +69,7 @@ var portfolio = [
     name: 'OGK Fest',
     slug: 'ogk',
     photo_url: '/src/images/portfolio/ogk.png',
-    excerpt: 'Resumo aqui',
-    description: '<p>We Places é uma ferramenta para facilitar a vida de quem procura espaços de cerimônia e festas.</p><p>O We Places conta com uma Single Page Application (SPA) que foi desenvolvida utilizando o VueJS 2 e é servida por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>A plataforma é uma aplicação multi clientes (Multi Tenancy) e é fornecida como SaaS (Software as a Service).</p><p>O We Places também conta com um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    description: '<p>OGK Fest é um evento que foi realizado em Presidente Prudente no final de fevereiro para o público geek de todas as idades, com cerca de 400 visitantes em sua primeira edição. Com diversas atrações diferenciadas, como sala escape, área medieval e talk show com youtubers, o site contava com informações sobre atrações e inscrições online para os concursos realizados no dia do evento.</p><p>O site é foi desenvolvido para ser um tema para Wordpress, deixando o desenvolvimento mais ágil e garantindo edições rápida do cliente para as próximas edições do evento</p>',
     company: 'Projeto Pessoal',
     link: 'http://ogkpro.com',
     tasks: [
@@ -88,8 +82,7 @@ var portfolio = [
     name: 'Valquiria Bastos - Arquiteta e Urbanista',
     slug: 'valquiria-bastos',
     photo_url: '/src/images/portfolio/valquiria-bastos.png',
-    excerpt: 'Resumo aqui',
-    description: '<p>We Places é uma ferramenta para facilitar a vida de quem procura espaços de cerimônia e festas.</p><p>O We Places conta com uma Single Page Application (SPA) que foi desenvolvida utilizando o VueJS 2 e é servida por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>A plataforma é uma aplicação multi clientes (Multi Tenancy) e é fornecida como SaaS (Software as a Service).</p><p>O We Places também conta com um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    description: '<p>O portfolio é algo extremamente importante na carreira de um profissional, e com a arquiteta Valquiria Bastos não foi diferente. Para mostrar seus cursos e trabalhos desenvolvidos, além de textos e resenhas em seu blog, foi criado um site portfolio diferente de  todos da região.</p><p>O site é foi desenvolvido para ser um tema para Wordpress, deixando o desenvolvimento mais ágil e garantindo edições rápida da cliente em uma plataforma na qual ela ja é acostumada.</p>',
     company: 'Projeto Pessoal',
     link: 'http://valquiriabastos.com/',
     tasks: [
@@ -97,13 +90,12 @@ var portfolio = [
     ]
   },
 
-  // OGK
+  // OGK Escape
   {
     name: 'OGK Fest - Escape 2018',
     slug: 'ogk-escape',
     photo_url: '/src/images/portfolio/ogk-escape.png',
-    excerpt: 'Resumo aqui',
-    description: '<p>We Places é uma ferramenta para facilitar a vida de quem procura espaços de cerimônia e festas.</p><p>O We Places conta com uma Single Page Application (SPA) que foi desenvolvida utilizando o VueJS 2 e é servida por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>A plataforma é uma aplicação multi clientes (Multi Tenancy) e é fornecida como SaaS (Software as a Service).</p><p>O We Places também conta com um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    description: '<p>A OGK Fest é um evento inovador em Presidente Prudente, com atrações diferenciadas voltadas para o público jovem e adulto com o tema geek. O jogo escape são casas de jogos “reais” em que os participantes têm um determinado tempo para tentar desvendar mistérios e sair de dentro de uma sala. Descobrir códigos para abrir cofres, decifrar enigmas químicos e passar por situações que envolvem a lógica fazem parte dessa nova onda... O aplicativo foi utilizado durante a missão OGK Fest, onde o objetivo era descobrir quem é e qual o objetivo de um desconhecido que está causando por todo o país.</p><p>O Aplicativo foi desenvolvido utilizando o VueJS e usa uma interface baseada em aplicativos de mensagens como WhatsApp, Telegram entre outros, onde a equipe conversava com o personagem desconhecido e através de dicas obtidas na sala, era possível descrobrir sua origem.</p><p>É um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android</p>',
     company: 'Projeto Pessoal',
     link: 'https://github.com/andrebrandaoo/ogk-escape-2018',
     tasks: [
@@ -116,8 +108,7 @@ var portfolio = [
     name: 'Dio Ferri',
     slug: 'dio-ferri',
     photo_url: '/src/images/portfolio/dio-ferri.png',
-    excerpt: 'Resumo aqui',
-    description: '<p>We Places é uma ferramenta para facilitar a vida de quem procura espaços de cerimônia e festas.</p><p>O We Places conta com uma Single Page Application (SPA) que foi desenvolvida utilizando o VueJS 2 e é servida por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>A plataforma é uma aplicação multi clientes (Multi Tenancy) e é fornecida como SaaS (Software as a Service).</p><p>O We Places também conta com um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    description: '<p>A missão da marca Dio Ferri é surpreender e atender seus clientes com o que há de melhor em camisaria. Além disso, eles também buscam o melhor no setor online, com um site desenvolvido com o charme e o profissionalismo da marca.</p><p>O Frontend foi desenvolvido com um Setup bem interessante ultilizando o Gulp e alguns pré-processadores famosos no mercado como: o stylus para css e pug para html.</p>',
     company: 'FG1',
     link: 'http://dioferricamisaria.com.br',
     tasks: [
@@ -130,8 +121,7 @@ var portfolio = [
     name: 'Atlântida - Assistência Técnica',
     slug: 'atlantida',
     photo_url: '/src/images/portfolio/atlantida.png',
-    excerpt: 'Resumo aqui',
-    description: '<p>We Places é uma ferramenta para facilitar a vida de quem procura espaços de cerimônia e festas.</p><p>O We Places conta com uma Single Page Application (SPA) que foi desenvolvida utilizando o VueJS 2 e é servida por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>A plataforma é uma aplicação multi clientes (Multi Tenancy) e é fornecida como SaaS (Software as a Service).</p><p>O We Places também conta com um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    description: '<p>A empresa oferece há mais de 35 anos serviços de manutenção e conserto de máquina de lavar, secadoras, lava e seca e geladeira em a toda Curitiba e Região. O site busca trazer informações úteis para os clientes e também um meio de contato para dúvidas e agendamentos.</p><p>O Frontend foi desenvolvido com um Setup bem interessante ultilizando o Gulp e alguns pré-processadores famosos no mercado como: o stylus para css e pug para html.</p>',
     company: 'FG1',
     link: 'false',
     tasks: [
@@ -139,13 +129,12 @@ var portfolio = [
     ]
   },
 
-  // Atlântida
+  // Das Humanas
   {
     name: 'Das Humanas',
     slug: 'das-humanas',
     photo_url: '/src/images/portfolio/das-humanas.png',
-    excerpt: 'Resumo aqui',
-    description: '<p>We Places é uma ferramenta para facilitar a vida de quem procura espaços de cerimônia e festas.</p><p>O We Places conta com uma Single Page Application (SPA) que foi desenvolvida utilizando o VueJS 2 e é servida por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>A plataforma é uma aplicação multi clientes (Multi Tenancy) e é fornecida como SaaS (Software as a Service).</p><p>O We Places também conta com um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    description: '<p>Das Humanas é o local certo para você encontrar suporte para os seus estudos pré-vestibulares, com questões e aulas em vídeos. É uma iniciativa do professor Daniel Medeiros, professor Mestre e Doutor que está no campo de ensino há mais de 35 anos.</p><p>O Frontend foi desenvolvido com um Setup bem interessante ultilizando o Gulp e alguns pré-processadores famosos no mercado como: o stylus para css e pug para html.</p>',
     company: 'FG1',
     link: 'https://dashumanas.com.br',
     tasks: [
@@ -158,8 +147,7 @@ var portfolio = [
     name: 'LC Soldas',
     slug: 'lc-soldas',
     photo_url: '/src/images/portfolio/lc-soldas.png',
-    excerpt: 'Resumo aqui',
-    description: '<p>We Places é uma ferramenta para facilitar a vida de quem procura espaços de cerimônia e festas.</p><p>O We Places conta com uma Single Page Application (SPA) que foi desenvolvida utilizando o VueJS 2 e é servida por uma Rest API Laravel, o banco de dados é o MySQL, a infra-estrutura é hospedada na Amazon Web Services AWS e Digital Ocean.</p><p>A plataforma é uma aplicação multi clientes (Multi Tenancy) e é fornecida como SaaS (Software as a Service).</p><p>O We Places também conta com um aplicativo hibrido onde foi utilizado o Cordova para gerar os aplicativos para Android e iOS.</p>',
+    description: '<p>A empresa oferece soluções práticas em todos os setores industriais, trabalhando com venda, manutenção locação de máquinas de solda para todos os processo. Por isso, um site informativo e completo conversa com a política de qualidade da empresa, buscando sempre o melhor para seus clientes.</p><p>O Frontend foi desenvolvido com um Setup bem interessante ultilizando o Gulp e alguns pré-processadores famosos no mercado como: o stylus para css e pug para html.</p>',
     company: 'FG1',
     link: false,
     tasks: [
