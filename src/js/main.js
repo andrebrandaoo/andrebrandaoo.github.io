c24743b810bb1ac51f0eda2bc536686c8a5e6b5d
Andre Brandao <andrebf4@gmail.com>
2018-03-11 17:55:20 -0300

    improvements in validInput and messagens with errors and succes

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,12 @@ new Vue({
     return {
       currentJob: null,
       interactions: {
-        modalJob: false
+        modalJob: false,
+        messageSuccess: false
       },
+      nameInvalid: false,
       emailInvalid: false,
-      errors: [],
+      msgInvalid: false,
       form: { name: '', email: '', msg: '', }
     }
   },
@@ -32,33 +34,35 @@ new Vue({
       this.interactions.modalJob = true
     },
 
-    validInput: function (input) {
-      var index = this.errors.indexOf(event.target.id)
+    validInput: function (event) {
+
+      // Check Name
+      if (event.target.id === 'name') {
+        this.nameInvalid = event.target.value === '' ? true : false
+      }
 
       // Check if email is valid
       if (event.target.id === 'email') {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-        this.emailInvalid = !re.test(event.target.value)
+        this.emailInvalid = event.target.value === '' || !re.test(event.target.value) ? true : false
       }
 
-      if ((!event.target.value || this.emailInvalid) && index < 0) {
-        this.errors.push(event.target.id)
-      }
-
-      if (event.target.value && index > -1) {
-        this.errors.splice(index, 1)
+      // Check Message
+      if (event.target.id === 'msg') {
+        this.msgInvalid = event.target.value === '' ? true : false
       }
     },
 
     submitForm: function () {
-      console.log(this.form)
-
       mixpanel.track('Contact Form', {
         'name': this.form.name,
         'email': this.form.email,
         'message': this.form.msg
       })
+
+      this.interactions.messageSuccess = true
+      this.form = { name: '', email: '', msg: '', }
     }
   }
 })
